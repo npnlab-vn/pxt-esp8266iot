df3ebe0b322833f25737c48fbbc32d0cea9c74cb
nhanksd85 <30594848+nhanksd85@users.noreply.github.com>
2021-07-10 11:17:32 +0700

    1

--- ESP8266.ts
+++ ESP8266.ts
@@ -70,8 +70,8 @@ namespace ESP8266_IoT {
     */
     //% block="Kết nối Wifi SSID = %ssid|KEY = %pw"
     //% group=ESP8266
-    //% ssid.defl=Tên mạng
-    //% pw.defl=Mật khẩu weight=95
+    //% ssid.defl=Tên_Mạng
+    //% pw.defl=Mật_Khẩu weight=95
     export function connectWifi(ssid: string, pw: string) {
         wifi_connected = false
         thingspeak_connected = false
