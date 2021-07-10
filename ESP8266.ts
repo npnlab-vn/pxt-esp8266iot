f97722c1648704798e8f1b35d4f9947d679a3574
nhanksd85 <30594848+nhanksd85@users.noreply.github.com>
2021-07-10 11:01:44 +0700

    123

--- ESP8266.ts
+++ ESP8266.ts
@@ -49,6 +49,7 @@ namespace ESP8266_IoT {
     * Initialize ESP8266 module 
     */
     //% block="Cài đặt ESP8266|RX %tx|TX %rx|Baud rate %baudrate"
+    //% group = "ESP8266"
     //% tx.defl=SerialPin.P8
     //% rx.defl=SerialPin.P12
     //% ssid.defl=your_ssid
@@ -67,6 +68,7 @@ namespace ESP8266_IoT {
     * connect to Wifi router
     */
     //% block="Kết nối Wifi SSID = %ssid|KEY = %pw"
+    //% group = "ESP8266"
     //% ssid.defl=your_ssid
     //% pw.defl=your_pw weight=95
     export function connectWifi(ssid: string, pw: string) {
@@ -105,6 +107,7 @@ namespace ESP8266_IoT {
     * Wait between uploads
     */
     //% block="Đợi kết nối %delay ms"
+    //% group = "ESP8266"
     //% delay.min=0 delay.defl=5000 weight=90
     export function wait(delay: number) {
         if (delay > 0) basic.pause(delay)
@@ -114,6 +117,7 @@ namespace ESP8266_IoT {
     * Check if ESP8266 successfully connected to Wifi
     */
     //% block="Kết nối Wifi %State" weight=85
+    //% group = "ESP8266"
     export function wifiState(state: boolean) {
         if (wifi_connected == state) {
             return true
@@ -127,6 +131,7 @@ namespace ESP8266_IoT {
     * Connect to ThingSpeak
     */
     //% block="Kết Nối ThingSpeak"
+    //% group = "ThingSpeak"
     //% write_api_key.defl=your_write_api_key 
     //% weight = 80
     export function connectThingSpeak() {
@@ -164,8 +169,9 @@ namespace ESP8266_IoT {
     * Connect to ThingSpeak and set data. 
     */
     //% block="Cấu hình dữ liệu | Write API key = %write_api_key|Field 1 = %n1||Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
-    //% write_api_key.defl=your_write_api_key 
-    //% weight = 5
+    //% group = "ThingSpeak"
+    //% write_api_key.defl=your_write_api_key
+    //% expandableArgumentMode="enabled" weight = 75
     export function setData(write_api_key: string, n1: number = 0, n2: number = 0, n3: number = 0, n4: number = 0, n5: number = 0, n6: number = 0, n7: number = 0, n8: number = 0) {
         toSendStr = "GET /update?api_key="
             + write_api_key
@@ -185,5 +191,71 @@ namespace ESP8266_IoT {
             + n7
             + "&field8="
             + n8
+    }
+    function waitUPTSResponse(): boolean {
+        let serial_str: string = ""
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200)
+                serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.includes("SEND OK")) {
+                result = true
+                break
+            }
+            else if (input.runningTime() - time > 10000) {
+                break
+            }
+        }
+        return result
+    }
+    /**
+    * upload data. It would not upload anything if it failed to connect to Wifi or ThingSpeak.
+    */
+    //% block="Gửi dữ liệu lên ThingSpeak"
+    //% group = "ThingSpeak"
+    //% weight = 70
+    export function uploadData() {
+        if (thingspeak_connected) {
+            last_upload_successful = false
+            sendAT("AT+CIPSEND=" + (toSendStr.length + 2), 100)
+            sendAT(toSendStr, 100) // upload data
+            last_upload_successful = waitUPTSResponse()
+            basic.pause(100)
+        }
+    }
+
+
+
+    /**
+    * Check if ESP8266 successfully connected to ThingSpeak
+    */
+    //% block="Kết nối ThingSpeak %State" 
+    //% group = "ThingSpeak"
+    //% weight = 65
+    export function thingSpeakState(state: boolean) {
+        if (thingspeak_connected == state) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+
+    /**
+    * Check if ESP8266 successfully uploaded data to ThingSpeak
+    */
+    //% block="Gửi dữ liệu ThingSpeak %State" 
+    //% group = "ThingSpeak"
+    //% weight = 60
+    export function tsLastUploadState(state: boolean) {
+        if (last_upload_successful == state) {
+            return true
+        }
+        else {
+            return false
+        }
     }
 }
